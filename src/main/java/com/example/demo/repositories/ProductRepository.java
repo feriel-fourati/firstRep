0fb79b16a0ec884770c7174b08d713a7f944ced1
feriel-fourati <feriel.fouratii@gmail.com>
2018-02-22 13:50:55 +0100

    third commit

--- src/main/java/com/example/demo/repositories/ProductRepository.java
+++ src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.Product;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Long>{
 
 }
